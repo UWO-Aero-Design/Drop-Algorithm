315d065ea7cb5833c9bf15a61ce3dfd2162f6829
samophthoner <xzhan982@uwo.ca>
2017-01-25 20:04:54 -0500

    Add files via upload
    this is the main function with all algorithms for dropping

--- Neural Network Demo/sim.py
+++ Neural Network Demo/sim.py
@@ -1,25 +1,36 @@
-def simulate_drop(m, x, y, z, vx, vy, vz, dx, dy, dz, wx, wy, wz, step = 0.01):
-    t = 0
-    g = 9.81
+from my_math import *
 
-    while z > 0:
+def simulate_drop(mass,  pos_self_current,  speed_current,  drag,  wind,  pos_goal,  step = 0.001):
 
-        t += step
-        
-        fx = dx * (vx-wx)**2
-        fy = dy * (vy-wy)**2
-        fz = dz * (vz-wz)**2 - g
+    time = 0
+    force = [0, 0, 0]
+    g = -9.806  # in Ottawa (g differs in different locations on earth)
+    acceleration = [0, 0, 0]
+    pos_self = pos_self_current
+    speed = speed_current
+    while pos_self[2] > 0:
+        time += step
 
-        ax = fx / m
-        ay = fy / m
-        az = fz / m
+        # in 3 dimensions
+        for counter in range(3):
+            force[counter] = drag[counter] * (speed[counter] - wind[counter])**2
+            if counter == 2:
+                force[counter] += g
+            acceleration[counter] = force[counter] / mass
+            pos_self[counter] += speed[counter] * step + 0.5*acceleration[counter] * step**2
+            speed[counter] += acceleration[counter] * step
 
-        x += vx*step + 0.5*ax*step**2
-        y += vy*step + 0.5*ay*step**2
-        z += vz*step + 0.5*az*step**2
+    # angle from moving direction to the direction to aim in the clockwiseness of unit circle in degrees
+    angle = calc_angle_diff_between(speed_current,   vector_add(pos_goal,pos_self_current,"-") )
+    return [time, pos_self, angleToDirect(angle), vector_add(pos_goal,pos_self,"-")]
+    # output: [
+    #
+    #            supposed length of time from dropping moment(now) to landing moment if it is dropped at current moment,
+    #            [x,y,z](supposed landing position if it is dropped at current moment ),
+    #            [direction from current velocity to aim direction, degree]  human readable
+    #            [x,y,z] distance from supposed landing position to aim if it is dropped at current moment
+    #
+    #          ]
 
-        vx += ax*step
-        vy += ay*step
-        vz += az*step
 
-    return (t, x, y, z)
+
